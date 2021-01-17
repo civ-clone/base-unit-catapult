573a6933371d59dbf9da01117d2ad4317c810f83
Dom Hastings <dom111@users.noreply.github.com>
2021-01-17 21:16:39 +0000

    Initial commit

---
+++ Catapult.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Catapult = void 0;
+const Fortifiable_1 = require("@civ-clone/base-unit-type-fortifiable/Fortifiable");
+class Catapult extends Fortifiable_1.Fortifiable {
+}
+exports.Catapult = Catapult;
+exports.default = Catapult;
+//# sourceMappingURL=Catapult.js.map
---
+++ README.md
@@ -0,0 +1,4 @@
+# base-unit-catapult
+
+This is a template repo.
+
